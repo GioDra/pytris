fab34d4b9f25e426cfbdd8455348c5a2fae1c2e5
GioDra <GioD@24dcaea7d349>
2018-06-07 09:24:54 +0000

    Creata una classe che contenga le funzioni della partita (Partita), per il momento il ciclo del main è un loop infinito e la classe Partita non ha ancora tutti i metodi necessari, ma c'è l'alternanza dei turni

--- pytris/__main__.py
+++ pytris/__main__.py
@@ -1,5 +1,5 @@
 import sys
-
+from turno import Partita
 
 
 def main(args=None):
@@ -7,7 +7,11 @@ def main(args=None):
     p1= input('Inserire il nome del primo giocatore: ')
     p2= input('Inserire il nome del secondo giocatore: ')
     print('Nomi giocatori: ',p1 ,p2)
+    terminata = False
+    t = Partita(p1,p2)
 
+    while terminata != True:
+        t.Turno()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- pytris/turno.py
+++ pytris/turno.py
@@ -1,11 +1,21 @@
-class Partita:
+class Partita(object):
     p1=""
     p2=""
+    change = True
+
 
     def __init__(self,p1,p2):
         self.p1=p1
         self.p2=p2
 
-    def turno(self):
-        print(self.p1, ' è il tuo turno')
-        print(self.p2, ' è il tuo turno')
+    def Mossa(self,p):
+        print("Posiziona qualcosa in una casella")
+
+    def Turno(self):
+        if(self.change == True):
+            p = self.p1
+        else:
+            p = self.p2
+        print(p, ' è il tuo turno')
+        self.Mossa(p)
+
